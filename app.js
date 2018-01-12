e2ecd7c3842e11d8a3796f0fff8f2063f2109085
detoxfarm3 <dey.ronak@gmail.com>
2018-01-12 22:21:41 +0530

    test

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-function hello() {
-    console.log('hello');
-    process.exit(0);
+async function logs() {
+    await exec();
+};
+
+async function exec() {    
+    let i = 100;
+    while(i > 0){
+        console.log('My name is ronak dey ' + i );
+        --i;
+    }
+    console.log('done')
 }
 
-hello();
+logs();
